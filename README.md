95fa9d8d436a78384d423f62dd397d0a73bc0f10
mastavtsev <mastavtsev@edu.hse.ru>
2024-11-03 16:05:53 +0300

    First commit for module-0

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-2e0aaae1b6195c2367325f4f02e2d04e9abb55f0b24a779b69b11b9e10269abc.svg)](https://classroom.github.com/online_ide?assignment_repo_id=16910679&assignment_repo_type=AssignmentRepo)
 # MiniTorch Module 0
 
 <img src="https://minitorch.github.io/minitorch.svg" width="50%">
@@ -6,3 +5,15 @@
 * Docs: https://minitorch.github.io/
 
 * Overview: https://minitorch.github.io/module0/module0/
+
+![Simple Dataset Classification](image.png)
+
+#### Parameters used:
+- `weight_0_0`: -2.53
+- `weight_1_0`: 0.48
+- `bias_0`: 0.54
+- `linear.weight_0_0`: -2.53
+- `linear.weight_1_0`: 0.48
+- `linear.bias_0`: 0.54
+
+P.S. ChatGPT was used to create docstring for functions.
--- minitorch/operators.py
+++ minitorch/operators.py
@@ -1,54 +1,322 @@
-"""Collection of the core mathematical operators used throughout the code base."""
+"""
+Collection of the core mathematical operators used throughout the code base.
+
+P.S. ChatGPT was used to generate docstring for functions
+"""
 
 import math
-
-# ## Task 0.1
 from typing import Callable, Iterable
 
-#
-# Implementation of a prelude of elementary functions.
+def mul(x: float, y: float) -> float:
+    """
+    Returns the product of two numbers x and y.
 
-# Mathematical functions:
-# - mul
-# - id
-# - add
-# - neg
-# - lt
-# - eq
-# - max
-# - is_close
-# - sigmoid
-# - relu
-# - log
-# - exp
-# - log_back
-# - inv
-# - inv_back
-# - relu_back
-#
-# For sigmoid calculate as:
-# $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$ if x >=0 else $\frac{e^x}{(1.0 + e^{x})}$
-# For is_close:
-# $f(x) = |x - y| < 1e-2$
+    Parameters:
+    - x: First number.
+    - y: Second number.
 
+    Returns:
+    - Product of x and y.
+    """
+    return x * y
 
-# TODO: Implement for Task 0.1.
+def id(x: float) -> float:
+    """
+    Returns the identity of x (i.e., x itself).
 
+    Parameters:
+    - x: Input value.
 
-# ## Task 0.3
+    Returns:
+    - x itself.
+    """
+    return x
 
-# Small practice library of elementary higher-order functions.
+def add(x: float, y: float) -> float:
+    """
+    Returns the sum of two numbers x and y.
 
-# Implement the following core functions
-# - map
-# - zipWith
-# - reduce
-#
-# Use these to implement
-# - negList : negate a list
-# - addLists : add two lists together
-# - sum: sum lists
-# - prod: take the product of lists
+    Parameters:
+    - x: First number.
+    - y: Second number.
 
+    Returns:
+    - Sum of x and y.
+    """
+    return x + y
 
-# TODO: Implement for Task 0.3.
+def neg(x: float) -> float:
+    """
+    Returns the negation of x.
+
+    Parameters:
+    - x: Input value.
+
+    Returns:
+    - Negated value of x.
+    """
+    return -x
+
+def lt(x: float, y: float) -> bool:
+    """
+    Returns True if x is less than y, otherwise False.
+
+    Parameters:
+    - x: First number.
+    - y: Second number.
+
+    Returns:
+    - Boolean indicating whether x < y.
+    """
+    return x < y
+
+def eq(x: float, y: float) -> bool:
+    """
+    Returns True if x is equal to y, otherwise False.
+
+    Parameters:
+    - x: First number.
+    - y: Second number.
+
+    Returns:
+    - Boolean indicating whether x == y.
+    """
+    return x == y
+
+def max(x: float, y: float) -> float:
+    """
+    Returns the maximum of two numbers x and y.
+
+    Parameters:
+    - x: First number.
+    - y: Second number.
+
+    Returns:
+    - Maximum of x and y.
+    """
+    return x if x > y else y
+
+def min(x: float, y: float) -> float:
+    """
+    Returns the minimum of two numbers x and y.
+
+    Parameters:
+    - x: First number.
+    - y: Second number.
+
+    Returns:
+    - Minimum of x and y.
+    """
+    return x if x < y else y
+
+def is_close(input: float, other: float, tol: float = 1e-2) -> bool:
+    """
+    Checks if two numbers are within a tolerance 'tol' of each other.
+
+    This function implements:
+    f(x, y) = |x - y| < tol
+
+    Parameters:
+    - input: First number for comparison.
+    - other: Second number for comparison.
+    - tol: Tolerance value. Default is 1e-2.
+
+    Returns:
+    - True if |input - other| < tol, otherwise False.
+    """
+    return abs(input - other) < tol
+
+def sigmoid(x: float) -> float:
+    """
+    Calculates the sigmoid of x with an optimized formula to avoid overflow.
+
+    This function implements:
+    f(x) = 1 / (1 + e^(-x)) if x >= 0, else e^x / (1 + e^x)
+
+    Parameters:
+    - x: The input value.
+
+    Returns:
+    - Sigmoid value of x.
+    """
+    return 1.0 / (1.0 + math.exp(-x)) if x > 0 else math.exp(x) / (1.0 + math.exp(x))
+
+def relu(x: float) -> float:
+    """
+    Applies the ReLU (Rectified Linear Unit) function on x.
+
+    Parameters:
+    - x: The input value.
+
+    Returns:
+    - x if x > 0, otherwise 0.
+    """
+    return x if x > 0 else 0.0
+
+def log(x: float) -> float:
+    """
+    Returns the natural logarithm of x.
+
+    Parameters:
+    - x: The input value.
+
+    Returns:
+    - Natural logarithm of x.
+    """
+    return math.log(x)
+
+def exp(x: float) -> float:
+    """
+    Returns e raised to the power of x.
+
+    Parameters:
+    - x: The input value.
+
+    Returns:
+    - Exponential of x.
+    """
+    return math.exp(x)
+
+def log_back(x: float, d: float) -> float:
+    """
+    Calculates the derivative of log(x) with respect to x, given an initial derivative d.
+
+    Parameters:
+    - x: Input value.
+    - d: Initial derivative with respect to the output of log(x).
+
+    Returns:
+    - The backpropagated derivative of log with respect to x.
+    """
+    return d / x
+
+def inv(x: float) -> float:
+    """
+    Returns the multiplicative inverse of x.
+
+    Parameters:
+    - x: Input value.
+
+    Returns:
+    - 1 / x.
+    """
+    return 1.0 / x
+
+def inv_back(x: float, d: float) -> float:
+    """
+    Calculates the derivative of 1/x with respect to x, given an initial derivative d.
+
+    Parameters:
+    - x: Input value.
+    - d: Initial derivative with respect to the output of 1/x.
+
+    Returns:
+    - The backpropagated derivative of the inverse with respect to x.
+    """
+    return -d / (x**2)
+
+def relu_back(x: float, d: float) -> float:
+    """
+    Returns the backpropagated derivative for ReLU given x and an initial derivative d.
+
+    Parameters:
+    - x: Input value.
+    - d: Initial derivative with respect to the output of ReLU.
+
+    Returns:
+    - d if x > 0, otherwise 0.
+    """
+    return d if x > 0 else 0.0
+
+def map(fn: Callable[[float], float], iter: Iterable[float]) -> Iterable[float]:
+    """
+    Applies function fn to each element in iter and returns a list of results.
+
+    Parameters:
+    - fn: Function to apply to each element.
+    - iter: Iterable of values to apply fn on.
+
+    Returns:
+    - List of results after applying fn to each element in iter.
+    """
+    return [fn(x) for x in iter]
+
+def zipWith(fn: Callable[[float, float], float], iter1: Iterable[float], iter2: Iterable[float]) -> Iterable[float]:
+    """
+    Applies a binary function fn to pairs from iter1 and iter2, returning a list of results.
+
+    Parameters:
+    - fn: Function to apply to pairs of elements from iter1 and iter2.
+    - iter1: First iterable of values.
+    - iter2: Second iterable of values.
+
+    Returns:
+    - List of results after applying fn to each pair from iter1 and iter2.
+    """
+    return [fn(x, y) for x, y in zip(iter1, iter2)]
+
+def reduce(fn: Callable[[float, float], float], iter: Iterable[float], init: float) -> float:
+    """
+    Reduces an iterable to a single value by iteratively applying a binary function fn.
+
+    Parameters:
+    - fn: Function to apply.
+    - iter: Iterable of values to reduce.
+    - init: Initial value for the reduction.
+
+    Returns:
+    - Reduced single value.
+    """
+    result = init
+    for x in iter:
+        result = fn(result, x)
+    return result
+
+def negList(lst: Iterable[float]) -> Iterable[float]:
+    """
+    Returns a list where each element of lst is negated.
+
+    Parameters:
+    - lst: Iterable of values to negate.
+
+    Returns:
+    - List of negated values.
+    """
+    return map(neg, lst)
+
+def addLists(lst1: Iterable[float], lst2: Iterable[float]) -> Iterable[float]:
+    """
+    Returns a list where each element is the sum of elements from lst1 and lst2 at corresponding positions.
+
+    Parameters:
+    - lst1: First iterable of values.
+    - lst2: Second iterable of values.
+
+    Returns:
+    - List of element-wise sums of lst1 and lst2.
+    """
+    return zipWith(add, lst1, lst2)
+
+def sum(lst: Iterable[float]) -> float:
+    """
+    Returns the sum of all elements in lst.
+
+    Parameters:
+    - lst: Iterable of values to sum.
+
+    Returns:
+    - Sum of all values in lst.
+    """
+    return reduce(add, lst, 0)
+
+def prod(lst: Iterable[float]) -> float:
+    """
+    Returns the product of all elements in lst.
+
+    Parameters:
+    - lst: Iterable of values to multiply.
+
+    Returns:
+    - Product of all values in lst.
+    """
+    return reduce(mul, lst, 1)
